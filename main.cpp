e8c03a872b4d96cd29e290536fd78a2b18cdeddc
Connor Shands-Sparks <connorshandssparks@gmail.com>
2024-03-23 16:24:21 -0700

    WIP

--- main.cpp
+++ main.cpp
@@ -31,11 +31,31 @@ int main() {
 bool first_pass(Symtab & symtab) 
 {
     printf("first\n") ;
+    // TODO: read all lines, looking for symbols, put length and value to symbol table
     return true;
 }
 
 bool second_pass(Symtab & symtab) 
 {
     printf("second\n") ;
+    // TODO: read each line and output assembly statement for each line
+    // TODO: handle start statement
+    // TODO: handle LDX
+    // TODO: handle LDA
+    // TODO: handle +LDB
+    // TODO: handle BASE
+    // TODO: handle ADD
+    // TODO: handle ADD
+    // TODO: handle TIX
+    // TODO: handle JLT
+    // TODO: handle +STA
+    // TODO: handle RSUB
+    // TODO: handle +LDA
+    // TODO: handle RESW
+    // TODO: handle RESW
+    // TODO: handle RESW
+    // TODO: handle RESW
+    // TODO: handle =C'EOF'
+    // TODO: handle END
     return true;
 }
