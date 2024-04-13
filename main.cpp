27d83a379fb75cb5b96a543bdaa591c8161421d0
Connor Shands-Sparks <connorshandssparks@gmail.com>
2024-04-13 16:53:36 -0700

    WIP

--- main.cpp
+++ main.cpp
@@ -44,16 +44,27 @@ bool first_pass(Symtab & /*symtab*/, string flnm)
     int run_total = 0;
     vector<string> holder;
     string op, operand;
+    char buffer[256];
+    std::vector<std::string> outlines;
+    
+    printf("%-8s%-8s%-8s%-8s%-8s\n", "CSect", "Symbol", "Value", "LENGTH", "Flags");
+    printf("--------------------------------------\n");
 
     while(std::getline(sicfile, line)) {
+        if (line[0] == '.') {
+            continue;
+        }
         char prefix = 0;
         char preop = 0;
         std::string label = get_label(line);
         std::string op = get_op(line, prefix);
         std::string operand = get_operand(line, preop);
         OpEntry entry = get_OpEntry(op, prefix);
+        std::string labelout, csectout = "";
+
         if (label.size() > 0) {
             symtab.values[label] = run_total;
+            labelout = label;
             /* cout << "label = " << label << " " << std::hex << run_total << "\n"; */
         }
         if (entry.codename == op){
@@ -76,24 +87,21 @@ bool first_pass(Symtab & /*symtab*/, string flnm)
             
         }
 
+    if (labelout != "") {
+        sprintf(buffer, "%-8s%-8s%06x%-8s%-8s\n", csectout.c_str(), labelout.c_str(), run_total, "", "F");
+        outlines.push_back(buffer);
+    }
+    }
+    sprintf(buffer, "%-8s%-8s%-8s%06x%-8s\n", "SUM", "", "", run_total, "");
+    outlines[0] = buffer;
+    for (auto s:outlines ) {
+        cout << s;
     }
 
-    printf("%-10s%-10s%-10s%-10s%-10s\n", "CSect", "Symbol", "Value", "LENGTH", "Flags");
-    printf("--------------------------------------\n");
-
-
-    /* while ()
-    {
-        if(output = addrentry)
-            output AddrEntry
-    } */
-    map<string, int>::iterator it;
-    for (it = symtab.values.begin(); it != symtab.values.end(); it++)
-    {
-        auto label = it->first;
-        auto location = it->second;
-        printf("%-15s%-15s%-15x%-15s%-15s\n", "", label.c_str(), location, "", "");
-    }
+    printf("\n");
+    printf("Literal Table\n");
+    printf("%-8s%-8s%-8s%-8s\n", "Name", "Operand", "Address", "Length:");
+    printf("--------------------------------\n");
 
     return true;
 }
@@ -109,9 +117,13 @@ bool second_pass(Symtab & /*symtab*/, string flnm)
     string op, operand;
     // TODO: Keep track of line number to pass to instruction
     while(std::getline(sicfile, line)) {
+        if (line[0] == '.') {
+            continue;
+        }
         Instruction instruct(line);
+
+        
         std::cout << instruct.op << " " << instruct.operand << std::endl;
-        // TODO: at this point i've passed the op and operand to Instruction
     }
 
     
